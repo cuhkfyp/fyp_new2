7ca19446704d7b2c913989835d90b0ba6ca607b7
final_year_project <94352630+cuhkfyp@users.noreply.github.com>
2022-04-12 15:23:18 +0800

    Add files via upload

---
+++ bc fyp.py
@@ -0,0 +1,61 @@
+def qFinder(C_arr, N_arr, new_w_locals, K,glob):
+    ret_arr=[]
+    for index in range(K):
+        #######get variable
+        array_x = []
+        flat_glob = []
+        iwLen = np.array(new_w_locals[index]['layer_input.weight'].cpu().numpy()).shape
+        ibLen = np.array(new_w_locals[index]['layer_input.bias'].cpu().numpy()).shape
+        hwLen = np.array(new_w_locals[index]['layer_hidden.weight'].cpu().numpy()).shape
+        hbLen = np.array(new_w_locals[index]['layer_hidden.bias'].cpu().numpy()).shape
+        iw = np.prod(iwLen)
+        ib = np.prod(ibLen)
+        hw = np.prod(hwLen)
+        hb = np.prod(hbLen)
+        d = iw + ib + hw + hb
+        target = 0
+        #####perform binary search
+        start = 0
+        end = d
+        while True:
+            midpoint = (end + start) // 2
+            check = math.log(comb(d, midpoint), 2) + 33 * midpoint
+            if (end - start == 1):
+                target = midpoint
+                break
+            elif check > N_arr[index] * C_arr[index]:
+                end = midpoint
+            else:
+                start = midpoint
+            # print(midpoint)
+
+        for item in new_w_locals[index].keys():
+            new_w_local_cpu=new_w_locals[index][item].cpu().numpy()
+            glob_cpu = glob[item].cpu().numpy()
+            k = new_w_local_cpu - glob_cpu
+            flat_glob = np.concatenate((flat_glob,glob_cpu),axis=None)
+            array_x = np.concatenate((array_x, k), axis=None)
+
+        abs_x = np.array(np.abs(array_x))
+        ind_that_set_to_zero = np.argpartition(abs_x, -target)[:-target]
+        ind_that_quantize = np.argpartition(abs_x, -target)[-target:]
+        array_x[ind_that_set_to_zero] = 0
+        max_ele = max(array_x)
+        min_ele = min(array_x)
+        a=(max_ele-min_ele)/2**32
+        array_x_temp = ((np.round((array_x - min_ele) / a)) * a) + min_ele
+        array_x_temp[ind_that_set_to_zero] = 0
+
+        if target == 0:
+            array_x_temp = flat_glob
+        elif target > 0:
+            array_x_temp = np.add(flat_glob, array_x_temp)
+
+        ret_arr.append(OrderedDict([
+            ('layer_input.weight', torch.FloatTensor((np.reshape(array_x_temp[:iw], iwLen)).tolist())),
+            ('layer_input.bias', torch.FloatTensor((np.reshape(array_x_temp[iw:iw + ib], ibLen)).tolist())),
+            ('layer_hidden.weight', torch.FloatTensor((np.reshape(array_x_temp[iw + ib:iw + ib + hw], hwLen)).tolist())),
+            ('layer_hidden.bias', torch.FloatTensor((np.reshape(array_x_temp[iw + ib + hw:], hbLen)).tolist()))
+        ]))
+
+    return ret_arr
